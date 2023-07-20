b134d2d60f43655da0171fc6e8cdd70905dc49a5
mohamedamineameur <134455223+mohamedamineameur@users.noreply.github.com>
2023-07-20 10:45:28 -0400

    5eme

--- js/aa.js
+++ js/aa.js
@@ -1,6 +1,6 @@
 var table = document.createElement("table");
 table.classList.add("tableau");
-var grille = document.getElementById("grille");
+var div = document.getElementById("div_grille");
 const btHaut = document.getElementById("haut");
 const btBas = document.getElementById("bas");
 const btGauche = document.getElementById("gauche");
@@ -10,9 +10,9 @@ var affs=document.getElementById("affScore")
 
 
 
-let table_monstrer=[]
+//let table_monstrer=[]
 
-for (let i = 0; i < 15; i++) {
+for (let i = 0; i < 15; i++) { // boucle qui créer un tableau html de 25td X 15tr
   var ligne = document.createElement("tr");
   ligne.classList.add("ligne_tableau");
 
@@ -20,25 +20,25 @@ for (let i = 0; i < 15; i++) {
     var colone = document.createElement("td");
     colone.classList.add("colone_tableau");
 
-    var image = document.createElement('img');
+    var image = document.createElement('img');// créer un element img qui sera le mur
     image.src = './img/img.png';
 
-    colone.appendChild(image);
-    ligne.appendChild(colone);
+    colone.appendChild(image); // ajout de l'image dans le td
+    ligne.appendChild(colone); // ajout du td dans le tr
   }
 
-  table.appendChild(ligne);
+  table.appendChild(ligne);// ajout du tr dans le tableau
 }
 
-grille.appendChild(table);
+div.appendChild(table);// ajout du tableau dans le div_grille
 
 let numAleatoire;
 
-function terrain() {
-  var numCelluleTerrain = []; // tableau qui contient les numero de cellule du terrain
+function terrain() { // fonction qui créer le sol du terrain en suprimant les image du mur
+  var numCelluleTerrain = []; // tableau qui contient les numero de cellule du sol du terrain
   let compteur = 0;
 
-  while (true) { // une boucle qui creer la premiere case du terrain aleatoirement entre 185 et 191
+  while (true) { // une boucle qui creer la premiere cellule du sol du terrain aleatoirement entre 185 et 191
     numAleatoire = Math.floor(Math.random() * 199);
     if (numAleatoire < 191 && numAleatoire > 185) {
       break;
@@ -46,9 +46,9 @@ function terrain() {
   }
 
 
-  numCelluleTerrain.push(numAleatoire);
+  numCelluleTerrain.push(numAleatoire); // ajout de la cellule au tableau des cellule
 
-  while (compteur < 60) { // une boucle qui creer 60 case du terrain aleatoirement en respectant l'obligation d'une presence d'une case qui soit a -1 ou +1 ou -25 ou + 25 dans le tableau
+  while (compteur < 60) { // une boucle qui creer 60 cellule sol de du terrain aleatoirement en respectant l'obligation d'une presence d'une cellule qui soit a -1 ou +1 ou -25 ou + 25 dans le tableau des cellule
     numAleatoire = Math.floor(Math.random() * 301);
     if (!numCelluleTerrain.includes(numAleatoire) && (numCelluleTerrain.includes(numAleatoire - 25) || numCelluleTerrain.includes(numAleatoire + 25) || numCelluleTerrain.includes(numAleatoire - 1) || numCelluleTerrain.includes(numAleatoire + 1))) {
       numCelluleTerrain.push(numAleatoire);
@@ -56,65 +56,74 @@ function terrain() {
     }
   }
 
-  for (let i = 0; i < 374; i++) { // une boucle pour creer le terrain en suprimant les image pésente dans le tableau numCelleTerrain
+  /* une fois la liste des cellule a été créée maintenant on va suprimer les images du mur qui sont dans les cellule présentes dans cette liste*/
+
+
+  for (let i = 0; i < 374; i++) { // une boucle pour creer le sol du terrain en suprimant les image pésente dans le tableau numCelleTerrain
     if (numCelluleTerrain.includes(i)) {
-      var cellules = document.querySelectorAll(".colone_tableau");
-      var imageASupprimer = cellules[i].querySelector("img");
+      var cellules = document.querySelectorAll(".colone_tableau");// variable qui stock les cellule de la grille
+      var imageASupprimer = cellules[i].querySelector("img"); // variable qui stock les element de type "img"
 
       
-        imageASupprimer.remove();
+        imageASupprimer.remove(); // commande pour suprimer cette image
       
     }
   }
 
-  return numCelluleTerrain;
+  return numCelluleTerrain; // retourner un tableau qui contient le numero des cellule du sol pour le réutiliser
 }
-var numTresor = [];
-var numMonster = [];
-var jeux = terrain();
-var joueur = []; 
-function jeu(jeux) {
+
+
+
+var numTresor = []; // tableau qui stock les cellules des tresor
+var numMonster = []; // tableau qui stock les cellule des monstre
+var jeux = terrain(); // variable qui stocke les cellule du sol de terrain obtenu de la fonction terrain()
+var joueur = [];  // variable qui stock la cellule du joueur
+
+
+
+function jeu(jeux) { // la fonction qui créer les monstres, les trésor et le joueur en fonction de la variable jeux obtenu de la fonction terrain
   
   
 
   let compteur2 = 0;
 
-  while (compteur2 < 3) {
-    numAleatoire = Math.floor(Math.random() * jeux.length);
-    let j = jeux[numAleatoire];
-    var cellules = document.querySelectorAll(".colone_tableau");
-    if (!numMonster.includes(j)) {
-      var image = document.createElement('img');
-      image.src = './img/monster.png';
-      cellules[j].appendChild(image);
-      numMonster.push(j)
-      table_monstrer.push([compteur2,j])
+  while (compteur2 < 3) { // boucle qui créer les monstre et le compteur2 est incrémenté selon une condition
+    numAleatoire = Math.floor(Math.random() * jeux.length); // choisir un nombre aleatoire parmis les index du tableau jeux
+    let j = jeux[numAleatoire];// récupérer une position dans le tableau jeux grace a l'indice aleatoire
+    var cellules = document.querySelectorAll(".colone_tableau");// stocker les td dans la variable cellule
+    if (!numMonster.includes(j)) {// verifier si cette position n'est pas deja prise
+      var image = document.createElement('img');// créer un element html de type img
+      image.src = './img/monster.png'; //importer l'image du monstre et la mettre dans l'element html de type img
+      cellules[j].appendChild(image);// ajouter cet element au td qui stockée dans la variable cellule 
+      numMonster.push(j)// stockage de la position monstre dans le tableau numMonstrer
+      //table_monstrer.push([compteur2,j])
       
-      compteur2++
-      console.log("monster " + (compteur2+1)+ " " + j);
+      compteur2++ // incrémenter le compteur
+      console.log("monster " + (compteur2+1)+ " " + j); // ligne de debogage
     }
   }
 
   
   let compteur3 = 0;
 
-  while (compteur3 < 3) {
-    numAleatoire = Math.floor(Math.random() * jeux.length);
-    let j = jeux[numAleatoire];
-    var cellules = document.querySelectorAll(".colone_tableau");
-    if (!numMonster.includes(j) && !numTresor.includes(j)) {
-      var image = document.createElement('img');
-      image.src = './img/tresor.png';
-      cellules[j].appendChild(image);
-      numTresor.push(j)
-      compteur3++
-      console.log("tresor " + j);
+  while (compteur3 < 3) {// boucle qui créer les trésors et le compteur3 est incrémenté selon une condition
+    numAleatoire = Math.floor(Math.random() * jeux.length);// choisir un nombre aleatoire parmis les index du tableau jeux
+    let j = jeux[numAleatoire];// récupérer une position dans le tableau jeux grace a l'indice aleatoire
+    var cellules = document.querySelectorAll(".colone_tableau");// stocker les td dans la variable cellule
+    if (!numMonster.includes(j) && !numTresor.includes(j)) {// verifier si cette position n'est pas deja prise
+      var image = document.createElement('img');// créer un element html de type img
+      image.src = './img/tresor.png'; //importer l'image du monstre et la mettre dans l'element html de type img
+      cellules[j].appendChild(image); // ajouter cet element au td qui stockée dans la variable cellule
+      numTresor.push(j)//stockage de la position du trésor dans le tableau numTrésor
+      compteur3++ // incrémentation du comteur
+      console.log("tresor " + j);// ligne de debogage
     }
   }
 
   let j;
   while (true) { // une boucle qui creer un emplacement du joueur aleatoirement selon le tableau du terrain
-    numAleatoire = Math.floor(Math.random() * (jeux.length - 1));
+    numAleatoire = Math.floor(Math.random() * (jeux.length ));
     j = jeux[numAleatoire];
     if (!numMonster.includes(j) && !numTresor.includes(j)) {
       break;
@@ -153,6 +162,7 @@ function haut() {
     cellules[position+25].querySelector("img").remove()
     joueur=[position]
     if(numTresor.includes(position)){
+      playSquareBipSound()
       cellules[position].querySelector("img").remove()
       
       let index= numTresor.indexOf(position)
@@ -183,6 +193,7 @@ function bas() {
       cellules[position-25].querySelector("img").remove()
       joueur=[position]
       if(numTresor.includes(position)){
+        playSquareBipSound()
         cellules[position].querySelector("img").remove()
         
         let index= numTresor.indexOf(position)
@@ -215,6 +226,7 @@ function gauche() {
       console.log(scorre)
 
       if(numTresor.includes(position)){
+        playSquareBipSound()
         cellules[position].querySelector("img").remove()
         
         let index= numTresor.indexOf(position)
@@ -243,6 +255,7 @@ function gauche() {
       cellules[position-1].querySelector("img").remove()
       joueur=[position]
       if(numTresor.includes(position)){
+        playSquareBipSound()
         cellules[position].querySelector("img").remove()
         
         let index= numTresor.indexOf(position)
@@ -279,7 +292,7 @@ function deplacementMonstre(){
         while(true){
         numAleatoire=Math.floor(Math.random()*4)
         let a=tableau_position[numAleatoire]
-        let h = table_monstrer[i][0]
+        
         let l = numMonster[i]
         console.log("voir: "+l)
         if (a==-1){
@@ -288,7 +301,7 @@ function deplacementMonstre(){
             numMonster[i]=l+a
 
             cellules[l].querySelector("img").remove()
-            console.log("apres deplacement du monstre :" + (i+1)+ " sa position est devenue: "+table_monstrer[i][1])
+            console.log("apres deplacement du monstre :" + (i+1)+ " sa position est devenue: "+numMonster[i])
             if(joueur==l+a){
                 alert("perdu")
                 window.location.reload(true);
@@ -302,7 +315,7 @@ function deplacementMonstre(){
             cellules[l+a].appendChild(image);
             numMonster[i]=l+a
             cellules[l].querySelector("img").remove()
-            console.log("apres deplacement du monstre :" + (i+1)+ " sa position est devenue: "+table_monstrer[i][1])
+            console.log("apres deplacement du monstre :" + (i+1)+ " sa position est devenue: "+numMonster[i])
             if(joueur==l+a){
                 alert("perdu")
                 window.location.reload(true);
@@ -319,7 +332,7 @@ function deplacementMonstre(){
             cellules[l+a].appendChild(image);
             numMonster[i]=l+a
             cellules[l].querySelector("img").remove()
-            console.log("apres deplacement du monstre :" + (i+1)+ " sa position est devenue: "+table_monstrer[i][1])
+            console.log("apres deplacement du monstre :" + (i+1)+ " sa position est devenue: "+numMonster[i])
             if(joueur==l+a){
                 alert("perdu")
                 window.location.reload(true);
@@ -338,7 +351,7 @@ function deplacementMonstre(){
             numMonster[i]=l+a
             cellules[l].querySelector("img").remove()
             
-            console.log("apres deplacement du monstre :" + (i+1)+ " sa position est devenue: "+table_monstrer[i][1])
+            console.log("apres deplacement du monstre :" + (i+1)+ " sa position est devenue: "+numMonster[i])
             if(joueur==l+a){
                 alert("perdu")
                 window.location.reload(true);
@@ -375,17 +388,21 @@ document.addEventListener("keydown", function(event){
     if(event.key==='ArrowUp'){
         haut();
         deplacementMonstre();
+        playBipSound();
         
     }
     else if (event.key === 'ArrowDown') {
         bas();
         deplacementMonstre();
+        playBipSound();
       } else if (event.key === 'ArrowLeft') {
         gauche();
         deplacementMonstre();
+        playBipSound();
       } else if (event.key === 'ArrowRight') {
         droite();
         deplacementMonstre();
+        playBipSound();
       }
 
       console.log("***************************************************")
@@ -393,55 +410,61 @@ document.addEventListener("keydown", function(event){
 
 affs.textContent=scorre;
 
-// function deplacementMonstre() {
-//   var cellules = document.querySelectorAll(".colone_tableau");
-//   var image = document.createElement('img');
-//   image.src = './img/monster.png';
-
-//   // Nouveau tableau pour stocker les nouvelles coordonnées des monstres après un déplacement
-//   var new_table_monstrer = [];
-
-//   for (let i = 0; i < table_monstrer.length; i++) {
-//     let h = table_monstrer[i][0];
-//     let l = table_monstrer[i][1];
-
-//     // Tableau des déplacements possibles : [-25, -1, 1, 25] (haut, gauche, droite, bas)
-//     let tableau_deplacements = [-25, -1, 1, 25];
-
-//     // Filtrer les déplacements valides en fonction de la position actuelle du monstre
-//     let deplacements_valides = tableau_deplacements.filter(deplacement => {
-//       let nouvellePosition = l + deplacement;
-//       return jeux.includes(nouvellePosition) && !numMonster.includes(nouvellePosition);
-//     });
-
-//     // Vérifier s'il y a des déplacements valides possibles pour le monstre
-//     if (deplacements_valides.length > 0) {
-//       // Choisir aléatoirement un déplacement valide parmi les possibilités
-//       let directionAleatoire = Math.floor(Math.random() * deplacements_valides.length);
-//       let deplacementChoisi = deplacements_valides[directionAleatoire];
-
-//       // Nouvelles coordonnées du monstre après le déplacement
-//       let nouvellePosition = l + deplacementChoisi;
-
-//       // Supprimer le monstre de sa position actuelle, s'il y a une image à supprimer
-//       let imageASupprimer = cellules[l].querySelector("img");
-//       if (imageASupprimer) {
-//         cellules[l].removeChild(imageASupprimer);
-//       }
-
-//       // Déplacer le monstre dans la nouvelle position
-//       cellules[nouvellePosition].appendChild(image);
-
-//       // Mettre à jour les coordonnées du monstre dans le nouveau tableau
-//       new_table_monstrer.push([h, nouvellePosition]);
-//     } else {
-//       // Si aucun déplacement valide n'est possible, le monstre reste à sa position actuelle
-//       new_table_monstrer.push([h, l]);
-//     }
-//   }
-
-//   // Remplacer l'ancien tableau par le nouveau tableau de coordonnées des monstres
-//   table_monstrer = new_table_monstrer;
-// }
 
 
+
+/************************************************************************************************************* */
+/************************************************************************************************************* */
+
+
+// toutes les fonction a partir de cette ligne sont tirées de ChatGPT
+
+
+function playBipSound() {
+  // Vérifier si l'API Web Audio est prise en charge par le navigateur
+  if (typeof AudioContext !== 'undefined' || typeof webkitAudioContext !== 'undefined') {
+      // Créer un contexte audio
+      var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+      // Créer un oscillateur pour générer le son
+      var oscillator = audioCtx.createOscillator();
+
+      // Connecter l'oscillateur au contexte audio
+      oscillator.connect(audioCtx.destination);
+
+      // Définir le type de forme d'onde du son (sinus ici pour un bip simple)
+      oscillator.type = 'sine';
+
+      // Définir la fréquence du son (en Hz)
+      oscillator.frequency.setValueAtTime(440, audioCtx.currentTime); // 440 Hz est le La de référence
+
+      // Démarrer l'oscillateur
+      oscillator.start();
+
+      // Arrêter l'oscillateur après 0.2 seconde (ajustez cette valeur selon vos préférences)
+      oscillator.stop(audioCtx.currentTime + 0.2);
+  } else {
+      // Si l'API Web Audio n'est pas prise en charge, affichez un message d'erreur
+      alert("L'API Web Audio n'est pas prise en charge par votre navigateur. Le bip sonore ne peut pas être joué.");
+  }
+}
+
+
+
+
+function playSquareBipSound() {
+  if (typeof AudioContext !== 'undefined' || typeof webkitAudioContext !== 'undefined') {
+      var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+      var oscillator = audioCtx.createOscillator();
+      oscillator.connect(audioCtx.destination);
+      
+      // Définir le type de forme d'onde du son (carrée cette fois)
+      oscillator.type = 'square';
+
+      oscillator.frequency.setValueAtTime(440, audioCtx.currentTime);
+      oscillator.start();
+      oscillator.stop(audioCtx.currentTime + 0.2);
+  } else {
+      alert("L'API Web Audio n'est pas prise en charge par votre navigateur. Le bip sonore ne peut pas être joué.");
+  }
+}
